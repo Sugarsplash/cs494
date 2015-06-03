88b33616312bfd7d81f2a7324b5428c781ab3c1e
Dylan Johnson <daj3@ada.cs.pdx.edu>
2015-06-02 19:50:28 -0700

    What I have so far

---
+++ dylan/IRC_client.py
@@ -0,0 +1,67 @@
+# Dylan Johnson
+# CS494
+# 6/3/15
+# IRC_client
+
+# This is a program for the client of an IRC
+
+# Modules
+import select	# Wait for I/O completion
+import socket	# Networking interface
+import string	# Useful for message displays
+import sys	# Command line arguments (i.e. argv)
+
+# User should supply the name of the program followed by the hostname (name of
+# the server), the port number, and then their username.
+# USAGE:	IRC_client.py <HOST> <PORT> <USERNAME>
+# EXAMPLE:	IRC_client.py ubuntu 1.2.3.4 my_username
+
+# Check if the user gave the correct number of arguments
+if len(sys.argv) < 4:
+    print "USAGE: IRC_client.py <HOST> <PORT> <USRENAME>";
+    sys.exit(0)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	# Create socket
+
+host = sys.argv[1]	# IRC server address
+port = int(sys.argv[2])	# IRC server port number
+username = sys.argv[3]	# Name of the user
+
+s.settimeout(3)		# This is needed for checking to see if the connection
+			# is still established (check if server is running)
+
+# Connect to the server
+s.connect((host, port))
+
+# Now that we're connected, make sure that the server can accept the username
+s.send(username)
+
+# Give the user a basic prompt interface
+sys.stdout.write("IRC-SERVER: ")
+sys.stdout.flush()	# Make sure to flush the buffer
+
+# The user is connected and can now pass in messages to the server. The server
+# will interpret the messages and determine which action the user wants to
+# take. These actions include 
+while 1:
+    # Create a list of input objects containing server socket and the
+    # standard input
+    input_objects = [s, sys.stdin]
+
+    # Check if any input sockets are ready (output and exception not used)
+    input_ready, output_dummy, exception_dummy = select.select(
+							input_objects, [], [])
+
+    # Check where the socket came from
+    for current_socket in input_ready:
+	if current_socket == s:		# Socket received from server
+	    info = current_socket.recv(2048)
+	    sys.stdout.write("IRC-SERVER: " + info + "\n")
+	    sys.stdout.flush()
+
+	else:				# Socket received from user
+	    user_message = sys.stdin.readline()
+	    s.send(user_message)
+	    sys.stdout.write("IRC-SERVER: ")
+	    sys.stdout.flush()
+
---
+++ dylan/IRC_server.py
@@ -0,0 +1,172 @@
+# Dylan Johnson
+# CS494
+# 6/3/15
+# IRC_server
+
+# This is a program for the server of an IRC
+
+# Modules
+import select	# Wait for I/O completion
+import socket	# Networking interface
+
+# Functions
+
+# This function determines what message the client sent to the server
+def decipher(client, info):
+    if info[0] == 'INROOM':
+	specified_room = info[len('INROOM'):]
+	INROOM(client, specified_room)
+    if info[0] == 'JOIN':
+	specified_room = info[len('JOIN'):]
+	JOIN(client, specified_room)
+    if info[0] == 'LEAVE':
+	specified_room = info[len('LEAVE'):]
+	LEAVE(client, specified_room)
+    if info[0] == 'LOGOUT':
+	LOGOUT(client)
+    if info[0] == 'PRIVMSG':
+	receiver = info[1]
+	message = info[(len('PRIVMSG')+len(receiver)):]
+	PRIVMSG(client, receiver, message)
+    if info[0] == 'ROOMS':
+	ROOMS(client)
+
+
+
+# This function sends server messages to all currently connected clients
+def shout(client, message):
+    for current_socket in connections:
+	# Make sure that the message isn't sent to the server or the client
+	# that sent the message
+	if current_socket != serv and current_socket != client:
+	    current_socket.send(message)
+
+
+
+# This function displays the names of all the clients in a particular room
+def INROOM(client, specified_room):
+    if len(rooms) == 0				# There are no rooms
+	client.send('There are currently no rooms to look into')
+    i = 0
+    for current_room in rooms:			# Check rooms
+	if current_room == specified_room:	# Room found
+	    for check in user_info:	# Send usernames in the room
+		if current_room in user_info[client]['rooms']:
+		    client.send(user_info[client]['username'])
+	    i = 1
+    if i == 0					# The room was not found
+	client.send('That room does not exist')
+
+
+
+# This function lets the client join a room, or create a room if the given
+# name of the room isn't in the list of current rooms
+def JOIN(client, specified_room):
+    if specified_room in rooms:
+	user_info[client]['rooms'].append(specified_room)
+	user_info[client]['current'] = specified_room
+	client.send(('Successfully joined %s') % specified_room)
+    else:				# Room not found, create room instead
+	rooms.append(specified_room)
+	user_info[client]['rooms'].append(specified_room)
+	user_info[client]['current'] = specified_room
+	client.send(('Created %s') % specified_room)
+
+
+
+# This function lets the client leave any room they have previously joined
+def LEAVE(client, specified_room)
+    if specified_room in user_info[client]['rooms']:
+	# If the room the client leaves is the room they're currently in,
+	# make sure to change their current room to no current room
+	if specified_room == user_info[client]['current']:
+	    user_info[client]['current'] = ''
+	user_info[client]['rooms'].remove(specified_room)
+    else:				# Client hasn't joined this room
+	client.send('You have not joined that room or it does not exist')
+
+
+
+# This function causes the client to exit the server
+def LOGOUT(client):
+    # Remove the user from any rooms previously joined
+    for i in range(len(user_info[client]['rooms'])):
+	LEAVE(client, user_info[client]['rooms'][i])
+    usernames.remove(user_info[client]['username'])# Remove client username
+    client.close()		# Close client connection
+    connections.remove(client)	# Remove client socket from connections
+
+
+
+# This function allows a client to send a private message to another client
+def PRIVMSG(client, receiver, message):
+    if receiver in usernames:
+	if receiver in connections:
+	    receiver.send(message)
+    else:		# Username not in server list
+	client.send('The given username was not found')
+
+
+
+# This function displays all the rooms currently in the server
+def ROOMS(client)
+   if len(rooms) == 0:		# Check if there are rooms on the server
+	client.send('There are no rooms on the server')
+   else:
+	client.send('Rooms in the server: ')
+	for current_room in rooms:
+	    client.send('%s, 'current_room)
+
+
+
+###############################################################################
+
+# Main program
+connections = []	# Monitor all socket connections
+port = 50000		# Port number
+rooms = []		# Array of rooms
+usernames = []		# Monitor list of usernames
+user_info = {}		# Required for obtaining details in user information
+
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create socket
+# set socket options here
+serv.bind(("0.0.0.0", port))	# bind the socket
+serv.listen(5)			# listen for clients
+
+# The server needs to be added as one of the connections
+connections.append(serv)
+
+while 1:
+    # Check if any input sockets are ready (output and exception not used)
+    input_ready, output_dummy, exception_dummy = select.select(
+							connections, [], [])
+
+    for client in input_ready:
+	if client == serv:	# Incoming connection
+	    clientfd, address = serv.accept()
+	    connections.append(client)
+	    # Set up for acquiring details of user information
+	    user_info[clientfd]={
+		'username': '',
+		'ip': '',		# Needed when searching for user info
+		'rooms': [],
+		'current': ''		# Keep track of which room client is in
+	    }
+	    # Make sure that the username sent isn't already in the server
+	    if name in usernames:
+		client.send('Username in use, log in with another username')
+		LOGOUT(client)
+	    elif ' ' in name:
+		client.send('Please do not use spaces in your username')
+		LOGOUT(client)
+	    else:
+		user_info[clientfd]['username'] = name
+	    user_info[clientfd]['ip'] = address[0]
+	else:				# Incoming message
+	    info = client.recv(2048)
+	    if info:
+		decipher(info)		# Interpret message
+	
+
+serv.close()
+
